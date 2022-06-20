7c77849eef4d23097694ec5fe234245c047131d3
KevinJudith <kjsmekens@gmail.com>
2022-06-20 14:31:48 +0800

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom';
+import { config, library } from '@fortawesome/fontawesome-svg-core';
+import { faEnvelope, faS, faChartSimple, faHeart, faHeartCircleBolt, faHouse, faUser} from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+// config.autoAddCss = false
+library.add(fab, faEnvelope, faS, faChartSimple, faHeart, faHeartCircleBolt, faHouse, faUser)
+
+const App = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
+// kindacode.com
